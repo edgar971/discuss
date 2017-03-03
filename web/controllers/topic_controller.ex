e5356fb03a10c6a73e4792d9399bd5718f94f0c6
Edgar Pino <me@edgarpino.com>
2017-03-02 20:44:17 -0600

    Added various routes and controller functions

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -4,10 +4,37 @@ defmodule Discuss.TopicController do
 
     alias Discuss.Topic
 
+    def index(conn, _params) do 
+        topics = Repo.all(Topic)
+        render conn, "index.html", topics: topics
+    end
+
     def new(conn, params) do 
         changeset = Topic.changeset(%Topic{}, %{})
         
-        render conn, "new.html"
+        render conn, "new.html", changeset: changeset
+    end
+
+    # We get something like "topic" => %{"title" => "Example"} as the 
+    # second parameter. Lets do some patern matching.
+    def create(conn, %{"topic" => topic}) do 
+        changeset = Topic.changeset(%Topic{}, topic)
+
+        case Repo.insert(changeset) do
+            {:ok, post} -> 
+                conn
+                |> put_flash(:info, "Topic Created")
+                |> redirect(to: topic_path(conn, :index))
+            {:error, changeset} -> 
+                render conn, "new.html", changeset: changeset
+        end
+
+    end
+
+    def edit(conn, %{"id" => topic_id}) do 
+        
+        
+
     end
 
 end
