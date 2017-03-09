bd9ab4726e023e2a70120a7da8b1ce3a3fb22e47
Edgar Pino <me@edgarpino.com>
2017-03-08 20:31:23 -0600

    Added edit form

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -41,7 +41,8 @@ defmodule Discuss.TopicController do
 
     def update(conn, %{"id" => topic_id, "topic" => topic}) do 
         
-        changeset = Repo.get(Topic, topic_id) |> Topic.changeset(topic)
+        old_topic = Repo.get(Topic, topic_id) 
+        changeset = Topic.changeset(old_topic, topic)
         
         case Repo.update(changeset) do
             {:ok, _topic} -> 
@@ -49,7 +50,7 @@ defmodule Discuss.TopicController do
                 |> put_flash(:info, "Topic updated")
                 |> redirect(to: topic_path(conn, :index))
             {:error, changeset} -> 
-                render conn, "edit.html", changeset: changeset
+                render conn, "edit.html", changeset: changeset, topic: old_topic 
         end
 
     end
