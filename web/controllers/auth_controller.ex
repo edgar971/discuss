0c86f2cea1c2f9b09fd0246436699b068a3679e2
Edgar Pino <me@edgarpino.com>
2017-03-19 20:54:50 -0500

    Added logout button and controller

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -4,9 +4,15 @@ defmodule Discuss.AuthController do
 
     def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
         
-        user_params = %{token: auth.credentials.token, email: auth.info.email, provider: "githug"}
+        user_params = %{token: auth.credentials.token, email: auth.info.email, provider: "github"}
         changeset = Discuss.User.changeset(%Discuss.User{}, user_params)
         sigin(conn, changeset)
+    end
+
+    def signout(conn, _params) do 
+        conn
+        |> configure_session(drop: true) # This way is more secure
+        |> redirect(to: topic_path(conn, :index))
     end
 
     defp sigin(conn, changeset) do 
